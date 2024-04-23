89493a653c382e08eb476be44ae6d376b269331a
Toranosuke01 <imakitos1023@gmail.com>
2024-04-24 03:44:49 +0900

    [update]introducing eslint and prettier

--- src/authSlice.js
+++ src/authSlice.js
@@ -4,20 +4,20 @@ import { Cookies } from "react-cookie";
 const cookie = new Cookies();
 
 const initialState = {
-  isSignIn: cookie.get("token") !== undefined
-}
+  isSignIn: cookie.get("token") !== undefined,
+};
 
 export const authSlice = createSlice({
   name: "auth",
   initialState,
   reducers: {
-    signIn: (state) => {
+    signIn: state => {
       state.isSignIn = true;
     },
-    signOut: (state) => {
+    signOut: state => {
       state.isSignIn = false;
     },
-  }
+  },
 });
 
 export const { signIn, signOut } = authSlice.actions;
